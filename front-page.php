017f615e9a77c9701ab950975355335667783f21
Aaron McNeal <aaron@pixotech.com>
2018-05-25 02:10:07 -0500

    Add front page template with color bar/picture and image directory

--- front-page.php
+++ front-page.php
@@ -2,6 +2,8 @@
     <main id="main">
         <div id="primary">
             <div id="content">
+                <div class="ft__color-bar"></div>
+                <div class="ft__front-banner-image"></div>
                 <?php if( have_posts() ): ?>
                     <?php while ( have_posts() ): the_post(); ?>
                         <?php get_template_part( 'content', get_post_format() ); ?>
--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 ### DEFINES ###
 define( 'FIREFLY_TEMPLATE_DIR', get_template_directory() );
-define( 'FIREFLY_STYLESHEET_DIR', get_stylesheet_directory() );
+define( 'FIREFLY_STYLESHEET_DIR', get_stylesheet_directory_uri() );
 define('INCLUDES', FIREFLY_TEMPLATE_DIR . '/includes');
 
 ### INCLUDES ###
@@ -22,12 +22,12 @@ add_action( 'init', 'firefly_register_menus' );
 
 ### STYLES AND SCRIPTS ###
 function firefly_register_styles_scripts() {
-	wp_register_style( 'firefly-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'firefly-style', get_stylesheet_uri() );
+	wp_register_style( 'firefly-style', FIREFLY_STYLESHEET_DIR . '/style.css' );
+	wp_enqueue_style( 'firefly-style', FIREFLY_STYLESHEET_DIR );
 
-    wp_enqueue_script( 'anime', get_template_directory_uri() . '/js/anime.min.js' );
-    wp_enqueue_script( 'jquery-min', get_template_directory_uri() . '/js/jquery-3.3.1.min.js' );
-    wp_enqueue_script( 'firefly-script', get_template_directory_uri() . '/js/script.js' );
+    wp_enqueue_script( 'anime', FIREFLY_STYLESHEET_DIR . '/js/anime.min.js' );
+    wp_enqueue_script( 'jquery-min', FIREFLY_STYLESHEET_DIR . '/js/jquery-3.3.1.min.js' );
+    wp_enqueue_script( 'firefly-script', FIREFLY_STYLESHEET_DIR . '/js/script.js' );
 }
 
 add_action( 'wp_enqueue_scripts', 'firefly_register_styles_scripts' );
