7b20639582ae53a20482f3c4346a0b0c8299d682
Aaron McNeal <aaron@pixotech.com>
2018-08-06 11:51:42 -0500

    Update front-page.php
    Update content div into section

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
     <main id="main">
         <div id="primary">
-            <div id="content">
+            <section id="content">
                 <div class="ft__color-bar"></div>
                 <div class="ft__front-banner-image"></div>
                 <?php if( have_posts() ): ?>
@@ -9,7 +9,7 @@
                         <?php get_template_part( 'content', get_post_format() ); ?>
                     <?php endwhile; ?>
                 <?php endif; ?>
-            </div>
+            </section>
         </div>
     </main>
 <?php get_footer(); ?>
