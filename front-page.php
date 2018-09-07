ee1775760ab141612ff20b1ed8b633258f7321c3
Aaron McNeal <aaron@pixotech.com>
2018-09-07 16:12:28 -0500

    Update front-page.php
    Adjust semantics

--- front-page.php
+++ front-page.php
@@ -1,15 +1,13 @@
 <?php get_header(); ?>
     <main id="main">
-        <div id="primary">
-            <section id="content">
-                <div class="ft__color-bar"></div>
-                <div class="ft__front-banner-image"></div>
-                <?php if( have_posts() ): ?>
-                    <?php while ( have_posts() ): the_post(); ?>
-                        <?php get_template_part( 'content', get_post_format() ); ?>
-                    <?php endwhile; ?>
-                <?php endif; ?>
-            </section>
-        </div>
+        <article id="content">
+            <div class="ft__color-bar"></div>
+            <section class="ft__front-banner-image"></section>
+            <?php if( have_posts() ): ?>
+                <?php while ( have_posts() ): the_post(); ?>
+                    <?php get_template_part( 'content', get_post_format() ); ?>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </article>
     </main>
 <?php get_footer(); ?>
