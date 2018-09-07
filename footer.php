5a277f2cdb7173f464d427af8935da87275ce609
Aaron McNeal <aaron@pixotech.com>
2018-09-07 16:18:17 -0500

    Update footer.php
    Update semantics

--- footer.php
+++ footer.php
@@ -1,28 +1,28 @@
-        </div>
+        </article>
 
-        <div class="ft__footer-container guttered">
+        <article class="ft__footer-container guttered">
             <footer class="ft__footer">
                 <?php if ( is_active_sidebar( 'firefly_footer_sidebar_one' ) ) : ?>
-                    <div id="footer-sidebar-one" class="footer-sidebar widget-area" role="complementary">
+                    <section id="footer-sidebar-one" class="footer-sidebar widget-area" role="complementary">
                         <?php dynamic_sidebar( 'firefly_footer_sidebar_one' ); ?>
-                    </div>
+                    </section>
                 <?php endif; ?>
                 <?php if ( is_active_sidebar( 'firefly_footer_sidebar_two' ) ) : ?>
-                    <div id="footer-sidebar-two" class="footer-sidebar widget-area" role="complementary">
+                    <section id="footer-sidebar-two" class="footer-sidebar widget-area" role="complementary">
                         <?php dynamic_sidebar( 'firefly_footer_sidebar_two' ); ?>
-                    </div>
+                    </section>
                 <?php endif; ?>
                 <?php if ( is_active_sidebar( 'firefly_footer_sidebar_three' ) ) : ?>
-                    <div id="footer-sidebar-three" class="footer-sidebar widget-area" role="complementary">
+                    <section id="footer-sidebar-three" class="footer-sidebar widget-area" role="complementary">
                         <?php dynamic_sidebar( 'firefly_footer_sidebar_three' ); ?>
-                    </div>
+                    </section>
                 <?php endif; ?>
             </footer>
 
-            <div class="ft__copyright-container guttered">
+            <section class="ft__copyright-container guttered">
                 <?php do_action( 'firefly_footer_copyright_bar' ); ?>
-            </div>
-        </div>
+            </section>
+        </article>
 
     </div>
 
